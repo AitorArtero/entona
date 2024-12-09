be2c8c7af5282886fae38361ee9452938c5df7ee
Aitor <aitor.artero@gmail.com>
2024-12-09 23:50:21 +0100

    añadida animación al apartado de buenas practicas por tema accesibilidad y añadido el disclaimer al apartado de simulador

--- src/pages/Simulator.jsx
+++ src/pages/Simulator.jsx
@@ -108,13 +108,20 @@ export function Simulator() {
             </button>
                 
             {/* Texto informativo */}
-            <div className="info-text-container">
-                <div className="info-text">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M19.114 5.636a9 9 0 0 1 0 12.728M16.463 8.288a5.25 5.25 0 0 1 0 7.424M6.75 8.25l4.72-4.72a.75.75 0 0 1 1.28.53v15.88a.75.75 0 0 1-1.28.53l-4.72-4.72H4.51c-.88 0-1.704-.507-1.938-1.354A9.009 9.009 0 0 1 2.25 12c0-.83.112-1.633.322-2.396C2.806 8.756 3.63 8.25 4.51 8.25H6.75Z" />
-                    </svg>
-                    <span>Para una experiencia más inmersiva utilice auriculares</span>
-                </div>
+            <div className="info-text">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M19.114 5.636a9 9 0 0 1 0 12.728M16.463 8.288a5.25 5.25 0 0 1 0 7.424M6.75 8.25l4.72-4.72a.75.75 0 0 1 1.28.53v15.88a.75.75 0 0 1-1.28.53l-4.72-4.72H4.51c-.88 0-1.704-.507-1.938-1.354A9.009 9.009 0 0 1 2.25 12c0-.83.112-1.633.322-2.396C2.806 8.756 3.63 8.25 4.51 8.25H6.75Z" />
+                </svg>
+                <span>Para una experiencia más inmersiva <strong>utilice auriculares</strong></span>
+            </div>
+
+            {/* Disclaimer */}
+            <div className="disclaimer">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z" />
+                </svg>
+                <p>Este simulador muestra una pequeña parte de la diversidad auditiva. Cada persona es un caso.</p>
+                <p>Si quieres saber más visita las <a href="/buenas-practicas">BUENAS PRÁCTICAS</a></p>
             </div>
         </div>
     );
